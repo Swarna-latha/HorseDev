fd00b2a5c38e306f45dde041d97d86c63f4b4e14
Aadhith Kalathil Bose <aadhi@fasteningcode.com>
2019-03-12 19:13:31 +1300

    nunit implemenetation

---
+++ 2019Feb/AdminstrationTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2019Feb
+{
+    class AdminstrationTests
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void setup()
+        {
+            // Creating a new Chrome Driver
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+
+            var loginPage = new LoginPage(driver);
+            loginPage.LoginSucess();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void CreateandValidateTimenMaterialTests()
+        {        
+            var home = new HomePage(driver);
+            home.ClickAdministration();
+            home.ClickTimenMaterials();
+
+            var time = new TimenMaterials(driver);
+            time.clickCreateNew();
+            time.EnterValidDetails();
+            time.ValidateRecord();           
+        }
+
+        [Test]
+        public void EditnValidate()
+        {
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+
+            //var loginPage = new LoginPage(driver);
+            //loginPage.LoginSucess();
+
+
+            var home = new HomePage(driver);
+            home.ClickAdministration();
+            home.ClickTimenMaterials();
+            //Create logc to edit and validate
+
+            //driver.Quit();
+        }
+
+        [Test]
+        public void DeletenValidate()
+        {
+
+            //IWebDriver driver = new ChromeDriver();
+            //driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
+
+            //var loginPage = new LoginPage(driver);
+            //loginPage.LoginSucess();
+
+
+            var home = new HomePage(driver);
+            home.ClickAdministration();
+            home.ClickTimenMaterials();
+
+            //Create logc to delete and validate
+
+            //driver.Quit();
+        }
+    }
+}
--- 2019Feb/Program.cs
+++ 2019Feb/Program.cs
@@ -8,25 +8,7 @@ namespace _2019Feb
     {
         static void Main(string[] args)
         {
-
-            // Creating a new Chrome Driver
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://horse-dev.azurewebsites.net/Account/Login?ReturnUrl=%2f");
-
-            var loginPage = new LoginPage(driver);
-            loginPage.LoginSucess();
-
-
-            var home = new HomePage(driver);
-            home.ClickAdministration();
-            home.ClickTimenMaterials();
-
-            var time = new TimenMaterials(driver);
-            time.clickCreateNew();
-            time.EnterValidDetails();
-            time.ValidateRecord();
-
-            //driver.Quit();
+                     
 
             
         }
