51295b7119485334f5e99364c543044241d719e7
Aadhith Kalathil Bose <aadhi@fasteningcode.com>
2019-03-25 19:50:59 +1300

    Specflow

---
+++ 2019Feb/StepsDefs/GoogleSearch.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace _2019Feb.StepsDefs
+{
+    [Binding]
+    public class GoogleSearchSteps
+    {
+        IWebDriver driver;
+        [Given(@"I am on Google Home page")]
+        public void GivenIAmOnGoogleHomePage()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+        
+        [When(@"I enter Specflow")]
+        public void WhenIEnterSpecflow()
+        {
+            driver.FindElement(By.Name("q")).SendKeys("Specflow");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+        }
+
+        [Then(@"I am taken to the Specflow results")]
+        public void ThenIAmTakenToTheSpecflowResults()
+        {
+            Thread.Sleep(2000);
+            Assert.AreEqual("Specflow - Google Search", driver.Title);
+            driver.Quit();
+        }
+    }
+}
---
+++ 2019Feb/Tests/GoogleSearch.feature
@@ -0,0 +1,6 @@
+﻿Feature: GoogleSearch
+
+Scenario:  A simple google search
+Given I am on Google Home page
+When I enter Specflow
+Then I am taken to the Specflow results 
